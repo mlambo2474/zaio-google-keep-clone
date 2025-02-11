ab9592262b75a9cc95d64d92bced6dc8d43ef4ce
Tonderai Mlambo <generaltonde@gmail.com>
2025-02-11 17:15:32 +0200

    initialised firebase, firebaseAuth and firebaseFirestore

--- app.js
+++ app.js
@@ -8,16 +8,19 @@ class Note {
 
 class App {
   constructor() {
-    this.notes =JSON.parse(localStorage.getItem('notes')) || [];
+//this.notes =JSON.parse(localStorage.getItem('notes')) || [];
+    this.notes = [];
     this.selectedNoteId = "";
     this.miniSidebar = true;
+    this.userId = "";
+
+    this.$form = document.querySelector(".form")
     this.$activeForm = document.querySelector(".active-form");
     this.$inactiveForm = document.querySelector(".inactive-form");
     this.$noteTitle = document.querySelector("#note-title");
     this.$noteText = document.querySelector("#note-text");
     this.$notes = document.querySelector(".notes");
    // this.$note = document.querySelector(".note")
-    this.$form = document.querySelector("#form");
     this.$modal = document.querySelector(".modal");
     this.$modalForm = document.querySelector("#modal-form")
     this.$modalTitle = document.querySelector("#modal-title");
@@ -25,21 +28,104 @@ class App {
     this.$closeModalForm = document.querySelector("#modal-button");
     this.$sidebar = document.querySelector(".sidebar");
     this.$sidebarActiveItem= document.querySelector(".active-item");
+    this.$app = document.querySelector("#app")
+    this.$firebaseAuthContainer = document.querySelector("#firebaseui-auth-container")
+    this.$authUserText =  document.querySelector(".auth-user")
+    this.$logOutButton = document.querySelector(".logout")
+    
+   
+    const firebaseConfig = {
+      apiKey: "AIzaSyBFep5gmWgDYx4H0XQRQ7ARDUjJDPaVOtk",
+      authDomain: "keep-616f4.firebaseapp.com",
+      projectId: "keep-616f4",
+      storageBucket: "keep-616f4.firebasestorage.app",
+      messagingSenderId: "443325408515",
+      appId: "1:443325408515:web:74022b72371c22fcf47536"
+    };
+    // Initialize Firebase
+    const app = firebase.initializeApp(firebaseConfig);
+    const auth = firebase.auth();
+    this.db = firebase.firestore();
+ 
+    console.log(firebase);
+    console.log(app);
+    console.log(auth);
 
-
-    // Initialize the FirebaseUI Widget using Firebase.
-    this.ui = new firebaseui.auth.AuthUI(auth);
-    this.ui.start('#firebaseui-auth-container', {
-      signInOptions: [
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
-      ],
-      // Other config options...
-    });
-
+  
+     this.ui = new firebaseui.auth.AuthUI(firebase.auth());
+     this.handleAuth();
+   
     this.addEventListeners();
     this.displayNotes();
   }
 
+  handleAuth(){
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/v8/firebase.User\
+        //  console.log(user.uid) to see if we are logged in
+         this.userId = user.uid
+        this.$authUserText.innerHTML = user.displayName;
+        this.redirectToApp();
+        this.ui.reset()
+      
+      } else {
+        // User is signed out
+        this.redirectToAuth();
+      }
+    });
+  }
+  
+  handleLogOut(){
+    firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+      this.redirectToAuth();
+      
+    }).catch((error) => {
+      // An error happened.
+      console.log("an error occured")
+    });
+  }
+  redirectToApp(){
+         this.$firebaseAuthContainer.style.display = "none";
+         this.$app.style.display ="block";
+         this.fetchNotesFromDB();    
+  }
+  
+  redirectToAuth(){
+       this.$firebaseAuthContainer.style.display = "block";
+       this.$app.style.display ="none";
+          
+        this.ui.start('#firebaseui-auth-container', {
+          callbacks: {
+            signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+              // User successfully signed in.
+              // Return type determines whether we continue the redirect automatically
+              // or whether we leave that to developer to handle.+
+              console.log(authResult.user.uid)
+              this.userId = authResult.user.uid;
+              this.$authUserText.innerHTML = authResult.user.displayName;
+              this.redirectToApp();
+             
+             return false // prevent automatic redirect
+            },
+          },
+           signInFlow: "redirect", 
+           signInOptions: [
+        {
+          provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          signInMethod: firebase.auth.EmailAuthProvider.EMAIL_PASSWORD_SIGN_IN_METHOD,
+          requireDisplayName: false 
+        },
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID
+       ],
+          // Other config options...
+          credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+          signInSuccessUrl: null, 
+        }) 
+    }
+   
   addEventListeners() {
     document.body.addEventListener("click", (event) => {
       this.handleFormClick(event);
@@ -67,8 +153,14 @@ class App {
     this.$sidebar.addEventListener("mouseout", () =>{
       this.handleToggleSidebar();
  })
+
+ this.$logOutButton.addEventListener("click", (event)=>{
+  this.handleLogOut()
+ })
 }
-  
+
+
+
 
   handleFormClick(event) {
     const isActiveFormClickedOn = this.$activeForm.contains(event.target);
@@ -138,21 +230,21 @@ class App {
 
   addNote({ title, text }) {
     if (text != "") {
-      const newNote = new Note(cuid(), title, text);
+      const newNote = {id: cuid(), title, text};
       this.notes = [...this.notes, newNote];
       this.render();
     }
   }
 
   editNote(id, { title, text }) {
-    this.notes = this.notes.map((note) => {
-      if (note.id == id) {
-        note.title = title;
-        note.text = text;
-      }
-      return note;
-    })
-    this.render()
+         this.notes = this.notes.map((note) => {
+           if (note.id == id) {
+             note.title = title;
+             note.text = text;
+           }
+           return note;
+         })
+         this.render()
   }
 
   handleMouseOverNote(element) {
@@ -173,8 +265,10 @@ class App {
 
   deleteNote(id) {
     this.notes = this.notes.filter((note) => note.id != id);
-   this.render();
+    this.render();
   }
+
+
   handleToggleSidebar(){
     if(this.miniSidebar){
       this.$sidebar.style.width = "180px";
@@ -186,16 +280,64 @@ class App {
        this.$sidebar.classList.remove("sidebar-hover")
        this.$sidebarActiveItem.classList.remove("sidebar-active-item")
        this.miniSidebar = true;
-    }
+     }
    }
-   saveNotes(){
-    localStorage.setItem('notes', JSON.stringify(this.notes));
+
+   fetchNotesFromDB(){
     
+         ///retrieving data 
+         var docRef = this.db.collection("users").doc(this.userId);
+          
+           docRef.get().then((doc) => {
+               if (doc.exists) {
+                   console.log("Document data:", doc.data().notes);
+                   this.notes = doc.data().notes;
+                   this.displayNotes();
+               } else {
+                   // doc.data() will be undefined in this case
+                   console.log("No such document!");
+                   console.log(this.userId)
+                  this.db.collection("users").doc("this.userId").set({
+                       notes: []
+                     })
+                     .then(() => {
+                       console.log("user successfully created!");
+                     })
+                     .catch((error) => {
+                       console.error("Error writing document: ", error);
+                     });
+               }
+           }).catch((error) => {
+               console.log("Error getting document:", error);
+           });
+
    }
+    
+   saveNotes(){
+            //localStorage.setItem('notes', JSON.stringify(this.notes));
+          //data added to firestore cloud
+         // Add a new document in collection "cities"
+        // console.log(this.$userId);
+     
+      console.log(this.userId)
+        //  const db = firebase.firestore();
+       this.db.collection("users").doc("this.userId").set({
+           notes: this.notes
+         })
+         .then(() => {
+           console.log("Document successfully written!");
+         })
+         .catch((error) => {
+           console.error("Error writing document: ", error);
+         });
+   }
+
+
    render(){
     this.saveNotes();
     this.displayNotes();
    }
+
   
   displayNotes() {
     this.$notes.innerHTML = this.notes
